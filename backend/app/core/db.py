5e08224193bb3784e3eb4bc01fefbca98773d5c0
ksiim <makskrutchev@gmail.com>
2024-11-16 17:51:19 +0500

    added settings env

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,8 +1,10 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 
+from backend.app.crud import settings
+
 from .config import *
-from app.models import User, SQLModel
+from app.models import Setting, User, SQLModel
 from app.crud.user import create_user
 
 
@@ -17,3 +19,8 @@ async def init_db(session: AsyncSession) -> None:
     # This works because the models are already imported and registered from app.models
     async with engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
+        
+    settings = (await session.execute(select(Setting))).scalars().all()
+    
+    if not settings:
+        
