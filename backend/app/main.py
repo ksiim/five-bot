bc717cbcd937354ec3676ddcd7517e6d92b2565f
ksiim <makskrutchev@gmail.com>
2024-11-23 22:34:15 +0500

    changed docs uri

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.routing import APIRoute
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.utils import get_openapi
 from starlette.middleware.cors import CORSMiddleware
 
 from app.api.main import api_router
@@ -14,6 +16,27 @@ app = FastAPI(
     title=PROJECT_NAME,
     generate_unique_id_function=custom_generate_unique_id,
 )
+
+app = FastAPI()
+
+@app.get("/api/v1/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="/static/swagger-ui-bundle.js",
+        swagger_css_url="/static/swagger-ui.css",
+    )
+
+@app.get("/api/v1/openapi.json", include_in_schema=False)
+async def get_openapi_json():
+    return get_openapi(
+        title=app.title,
+        version=app.version,
+        description=app.description,
+        routes=app.routes,
+    )
 
 cors = [str(origin) for origin in BACKEND_CORS_ORIGINS] + [
             FRONTEND_HOST
