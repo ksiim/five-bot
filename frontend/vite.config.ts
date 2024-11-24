23dce8a30a731af689201e1e089876539b17c4fd
mambbas <arzontum@gmail.com>
2024-11-24 12:56:00 +0500

    attempt to connect ton-wallet №7

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,7 +9,11 @@ export default defineConfig({
     },
   },
   define: {
-    global: {},
+    'global': {},
     'process.env': {},
+    'Buffer': ['buffer', 'Buffer'],
+  },
+  optimizeDeps: {
+    include: ['buffer'],
   },
 })
