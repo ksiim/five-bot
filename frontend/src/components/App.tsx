75a4968f15866121383cb3f124905429374ab171
mambbas <arzontum@gmail.com>
2024-11-24 12:42:54 +0500

    attempt to connect ton-wallet №4

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -9,6 +9,7 @@ import Tasks from '../pages/Tasks/Tasks';
 import {getUserByTelegramId} from '../services/userService';
 import { IUser } from '../interfaces/User.ts';
 import {TG} from '../api/request.ts';
+import './polyfills';
 
 function App() {
   const [user, setUser] = useState<IUser>({
