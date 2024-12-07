6d22223c5459d51c410419e6292ff3311f27db57
mambbas <arzontum@gmail.com>
2024-12-08 00:54:59 +0500

    hotfixing popup 3

--- frontend/src/components/BurgerMenu/BurgerMenu.tsx
+++ frontend/src/components/BurgerMenu/BurgerMenu.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './BurgerMenu.module.scss';
 import telegramIcon from './../../assets/images/telegramIcon.svg';
 import documentIcon from './../../assets/images/documentIcon.svg';
-import {TG} from '../../api/request.ts';
+import { TG } from '../../api/request.ts';
 import Button from '../Button/Button.tsx';
 
 interface BurgerMenuProps {
@@ -10,18 +10,50 @@ interface BurgerMenuProps {
 }
 
 const BurgerMenu: React.FC<BurgerMenuProps> = ({ onClose }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  
+  useEffect(() => {
+    setIsOpen(true); // Показываем поп-ап при монтировании
+    return () => setIsOpen(false); // Убираем поп-ап при размонтировании
+  }, []);
+  
+  const handleClose = () => {
+    setIsOpen(false); // Убираем поп-ап с анимацией
+    setTimeout(onClose, 400); // Закрываем через 400 мс (длительность анимации)
+  };
+  
   return (
-    <div className={styles.popup}>
-      <div className={styles.popupContent}>
-        <button className={styles.closeButton} onClick={onClose}>
-          &times;
-        </button>
-        <div className={styles.buttonGroup}>
-          <Button text = {'Наш телеграм канал'} icon={telegramIcon} onClick={() => TG.openTelegramLink('https://t.me/givefive_community')}/>
-          <Button text = {'Условия пользования'} icon={documentIcon} onClick={() => TG.openLink('https://telegra.ph/Standard-Bot-Privacy-Policy-for-givefive-bot-09-21')}/>
+    <>
+      <div
+        className={`${styles.popupOverlay} ${isOpen ? styles.open : ''}`}
+        onClick={handleClose}
+      ></div>
+      <div className={`${styles.popup} ${isOpen ? styles.open : ''}`}>
+        <div className={styles.popupContent}>
+          <button className={styles.closeButton} onClick={handleClose}>
+            &times;
+          </button>
+          <div className={styles.buttonGroup}>
+            <Button
+              text={'Наш телеграм канал'}
+              icon={telegramIcon}
+              onClick={() =>
+                TG.openTelegramLink('https://t.me/givefive_community')
+              }
+            />
+            <Button
+              text={'Условия пользования'}
+              icon={documentIcon}
+              onClick={() =>
+                TG.openLink(
+                  'https://telegra.ph/Standard-Bot-Privacy-Policy-for-givefive-bot-09-21',
+                )
+              }
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
